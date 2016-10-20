edc35c7abcef988187dbb5a713d219d2229aa4e3
Helmut Januschka <h.januschka@krone.at>
2016-10-20 00:13:41 -0700

    up

--- lib/fastlane/plugin/cryptex/runner.rb
+++ lib/fastlane/plugin/cryptex/runner.rb
@@ -2,6 +2,30 @@ module Fastlane
   module Cryptex
     class Runner
       attr_accessor :git_changed
+      
+      def import_env(params)
+        env_world_file = "#{params[:workspace]}/#{params[:key]}_env_world.crypt"
+        File.write(env_world_file, params[:hash].to_json)
+        @git_changed = true
+      end
+      def export_env(params)
+        env_world_file = "#{params[:workspace]}/#{params[:key]}_env_world.crypt"
+        world_data = File.read(env_world_file)
+        world_data_parsed = JSON.parse(world_data)
+          ret_json = {}
+          
+          world_data_parsed.keys.each do | el |
+            
+              if !params[:hash] || (params[:hash] && params[:hash].keys.include?(el))
+                ret_json[el] = world_data_parsed[el] 
+                if params[:set_env]
+                  ENV[el] = world_data_parsed[el]
+                end
+              end
+            
+        end        
+        return world_data_parsed
+      end
       def import_file(params)
         File.write("#{params[:workspace]}/#{params[:key]}.crypt", File.read(File.expand_path(params[:in])))
         @git_changed = true
@@ -31,6 +55,12 @@ module Fastlane
         @git_changed = false
         params[:workspace] = GitHelper.clone(params[:git_url], params[:shallow_clone], skip_docs: params[:skip_docs], branch: params[:git_branch])
         @params = params
+        if params[:type] == "import_env"
+          return import_env(params)
+        end
+        if params[:type] == "export_env"
+          return export_env(params)
+        end
         if params[:type] == "import"
           import_file(params)
           return
