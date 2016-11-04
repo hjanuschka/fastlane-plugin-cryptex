da3715aead8185adddf7d8db313acf5ebd1802f6
Helmut Januschka <helmut@januschka.com>
2016-11-04 20:12:58 +0100

    rubocop

--- spec/cryptex_action_spec.rb
+++ spec/cryptex_action_spec.rb
@@ -1,17 +1,15 @@
 module Fastlane
-  
-describe Fastlane::Actions::CryptexAction do
-  describe '#run' do
-    it 'prints a message' do
-#      expect(Fastlane::UI).to receive(:message).with("The cryptex plugin is working!")
-      values = {
-        git_url: "/tmp/123",
-        
-      }
-      config = FastlaneCore::Configuration.create(Cryptex::Options.available_options, values)
-#      Fastlane::Actions::CryptexAction.run(config)
+  describe Fastlane::Actions::CryptexAction do
+    describe '#run' do
+      it 'prints a message' do
+        #      expect(Fastlane::UI).to receive(:message).with("The cryptex plugin is working!")
+        values = {
+          git_url: "/tmp/123"
+
+        }
+        config = FastlaneCore::Configuration.create(Cryptex::Options.available_options, values)
+        #      Fastlane::Actions::CryptexAction.run(config)
+      end
     end
   end
-end
-
 end
